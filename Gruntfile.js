ad8fb08ec42e006fc2d434a7e76c51955ce2cf34
Julian Brendl <julianbrendl@gmail.com>
2016-05-18 23:59:23 +0200

    initial commit - installed grunt and added a template

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+/**
+ * Run sudo npm install to download all dependencies
+ */
+
+var pjson = require('./package.json');
+var version = pjson.version;
+
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        'gh-pages': {
+            options: {
+                base: 'Sedna'
+            },
+            src: ['**']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-gh-pages');
+
+
+    // Create the default task
+    grunt.registerTask('default', ['uglify']);
+};
