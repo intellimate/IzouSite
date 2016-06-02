fe6edf23d3550483c100b19b5b8d66a3e1a98eef
Julian Brendl <julianbrendl@gmail.com>
2016-06-02 17:45:59 +0200

    added basic webpack (almost working) setup

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,27 +5,27 @@ var path = require('path');
 module.exports = {
     context: path.join(__dirname, "src"),
     devtool: debug ? "inline-sourcemap" : null,
-    entry: "./js/index.jsx",
+    entry: "./js/index.js",
     module: {
         loaders: [
             {
                 test: /\.jsx?$/,
                 exclude: /(node_modules)/,
-                loader: 'babel-loader',
+                loader: "babel-loader",
                 query: {
                     presets: ['react', 'es2015'],
-                    plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
+                    plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
                 }
             }
         ]
     },
     output: {
-        path: __dirname + "/src/js",
+        path: __dirname + "/src",
         filename: "app.js"
     },
     plugins: debug ? [] : [
         new webpack.optimize.DedupePlugin(),
         new webpack.optimize.OccurenceOrderPlugin(),
         new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-    ],
+    ]
 };
