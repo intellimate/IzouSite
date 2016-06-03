825e6da1a944e58c090903d7782df1dc56e6937a
Julian Brendl <julianbrendl@gmail.com>
2016-06-03 16:20:41 +0200

    added eslint and turned prod build to dev build for now as there is no prod build yet

--- src/js/containers/AddTodo.js
+++ src/js/containers/AddTodo.js
@@ -2,29 +2,26 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { addTodo } from '../actions'
 
+/* eslint react/prop-types: 0 */
 let AddTodo = ({ dispatch }) => {
-  let input
+    let input
 
-  return (
+    return (
     <div>
-      <form onSubmit={e => {
-        e.preventDefault()
-        if (!input.value.trim()) {
-          return
-        }
-        dispatch(addTodo(input.value))
-        input.value = ''
-      }}>
-        <input ref={node => {
-          input = node
-        }} />
-        <button type="submit">
-          Add Todo
-        </button>
-      </form>
+        <form onSubmit = {e => {
+            e.preventDefault()
+            if (!input.value.trim()) {
+              return
+            }
+            dispatch(addTodo(input.value))
+            input.value = ''
+        }}>
+            <input ref={node => {input = node}} />
+            <button type="submit">Add Todo</button>
+        </form>
     </div>
-  )
+    )
 }
-AddTodo = connect()(AddTodo)
 
+AddTodo = connect()(AddTodo)
 export default AddTodo
